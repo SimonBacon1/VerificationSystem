969078b362f8c8e6ff0500192275c67aafc64ef1
pochirojas <srojasbon@gmail.com>
2025-12-12 16:59:27 -0500

    remove

--- services/resend.js
+++
@@ -1,6 +0,0 @@
-import { Resend } from 'resend';
-const resend = new Resend(process.env.RESEND_API_KEY);
-
-export async function sendEmail({ to, subject, html }){
-  return await resend.emails.send({ from: `Connabis <no-reply@${process.env.SHOPIFY_STORE || 'connabis.com.co'}>`, to, subject, html });
-}
