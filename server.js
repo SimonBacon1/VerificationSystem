0f45345bf9c4caae5cf3d8aba8d04435add765aa
pochirojas <srojasbon@gmail.com>
2026-01-17 03:23:51 -0500

    Fix

--- server.js
+++ server.js
@@ -2,11 +2,9 @@ import express from 'express';
 import { sendTestEmail } from './services/email.js';
 import { sendVerificationEmail } from './services/email.js';
 import sumaRouter from './routes/suma.js';
-import shopifyRouter from './routes/shopify.js';
 import cors from 'cors';
 import shopifyRouter from './routes/shopify.js';
 
-app.use('/shopify', shopifyRouter);
 
 app.use(cors());
 app.use('/suma', sumaRouter);
